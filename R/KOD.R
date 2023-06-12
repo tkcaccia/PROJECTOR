81f17df65ea0e778bae70342781c124bd8aae110
tkcaccia <tkcaccia@gmail.com>
2023-06-12 09:39:02 +0200

    Update KOD.R

--- R/KOD.R
+++ R/KOD.R
@@ -522,13 +522,14 @@ spatial.knn = 10)
                                  spatial.knn)$nn_index
       
     }
+    options(warn=-1)
     yatta=0
     attr(yatta,"class")="try-error"
     while(!is.null(attr(yatta,"class"))){
     yatta = try(core_cpp(x, xTdata, clbest, Tcycle, FUN, f.par, 
                      Xconstrain_ssa, Xfix_ssa, shake, xNeighbors))
     }
-
+    options(warn=0)
     if (is.list(yatta)) {
       clbest = as.vector(yatta$clbest)
       accu = yatta$accbest
