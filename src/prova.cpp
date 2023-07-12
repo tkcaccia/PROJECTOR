81c020d1e8e38e76eaf1298e9a33a4931786036f
tkcaccia <tkcaccia@gmail.com>
2023-07-12 13:28:36 +0200

    Update prova.cpp

--- src/prova.cpp
+++ src/prova.cpp
@@ -854,7 +854,7 @@ arma::ivec KNNPLSDACV(arma::mat x,arma::ivec cl,arma::ivec constrain,int k,arma:
     }
   }  
 
-  Ytest.elem( find(Ytest==0) ) = -999;
+  Ytest.elem( find(Ytest==0) ) -= 999.0;
   
   Rcout<<Ytest;
   int mm2=constrain.size();
